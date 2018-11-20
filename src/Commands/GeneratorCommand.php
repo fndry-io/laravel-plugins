5feec6b3b1e4c10ed38116c116efe7a13e7e79ef
Greg Gunner <greg@co-foundry.co.za>
2018-11-20 13:53:53 +0200

    Adjusting how the generator creates the directory to the plugin files

--- src/Commands/GeneratorCommand.php
+++ src/Commands/GeneratorCommand.php
@@ -8,101 +8,101 @@ use Nwidart\Modules\Generators\FileGenerator;
 
 abstract class GeneratorCommand extends Command
 {
-    /**
-     * The name of 'name' argument.
-     *
-     * @var string
-     */
-    protected $argumentName = '';
+	/**
+	 * The name of 'name' argument.
+	 *
+	 * @var string
+	 */
+	protected $argumentName = '';
 
-    /**
-     * Get template contents.
-     *
-     * @return string
-     */
-    abstract protected function getTemplateContents();
+	/**
+	 * Get template contents.
+	 *
+	 * @return string
+	 */
+	abstract protected function getTemplateContents();
 
-    /**
-     * Get the destination file path.
-     *
-     * @return string
-     */
-    abstract protected function getDestinationFilePath();
+	/**
+	 * Get the destination file path.
+	 *
+	 * @return string
+	 */
+	abstract protected function getDestinationFilePath();
 
-    /**
-     * Execute the console command.
-     */
-    public function handle()
-    {
-        $path = str_replace('\\', '/', $this->getDestinationFilePath());
+	/**
+	 * Execute the console command.
+	 */
+	public function handle()
+	{
+		$path = str_replace('\\', '/', $this->getDestinationFilePath());
 
-        if (!$this->laravel['files']->isDirectory($dir = dirname($path))) {
-            $this->laravel['files']->makeDirectory($dir, 0777, true);
-        }
+		if (!$this->laravel['files']->isDirectory($dir = dirname($path))) {
+			$this->laravel['files']->makeDirectory($dir, 0777, true);
+		}
 
-        $contents = $this->getTemplateContents();
+		$contents = $this->getTemplateContents();
 
-        try {
-            with(new FileGenerator($path, $contents))->generate();
+		try {
+			with(new FileGenerator($path, $contents))->generate();
 
-            $this->info("Created : {$path}");
-        } catch (FileAlreadyExistException $e) {
-            $this->error("File : {$path} already exists.");
-        }
-    }
+			$this->info("Created : {$path}");
+		} catch (FileAlreadyExistException $e) {
+			$this->error("File : {$path} already exists.");
+		}
+	}
 
-    /**
-     * Get class name.
-     *
-     * @return string
-     */
-    public function getClass()
-    {
-        return class_basename($this->argument($this->argumentName));
-    }
+	/**
+	 * Get class name.
+	 *
+	 * @return string
+	 */
+	public function getClass()
+	{
+		return class_basename($this->argument($this->argumentName));
+	}
 
-    /**
-     * Get default namespace.
-     *
-     * @return string
-     */
-    public function getDefaultNamespace() : string
-    {
-        return '';
-    }
+	/**
+	 * Get default namespace.
+	 *
+	 * @return string
+	 */
+	public function getDefaultNamespace() : string
+	{
+		return '';
+	}
 
-    /**
-     * Get class namespace.
-     *
-     * @param \Nwidart\Modules\Module $module
-     *
-     * @return string
-     */
-    public function getClassNamespace($module)
-    {
-        $extra = str_replace($this->getClass(), '', $this->argument($this->argumentName));
+	/**
+	 * Get class namespace.
+	 *
+	 * @param \Nwidart\Modules\Module $module
+	 *
+	 * @return string
+	 */
+	public function getClassNamespace($module)
+	{
+		$extra = str_replace($this->getClass(), '', $this->argument($this->argumentName));
 
-        $extra = str_replace('/', '\\', $extra);
+		$extra = str_replace('/', '\\', $extra);
 
-        $namespace = $this->laravel['modules']->config('namespace');
+		$namespace = $this->laravel['modules']->config('namespace');
 
-        $namespace .= '\\' . str_replace('_', '\\', $module->getStudlyName());
+		$namespace .= '\\' . str_replace('_', '\\', $module->getStudlyName());
 
-        $namespace .= '\\' . $this->getDefaultNamespace();
+		$namespace .= '\\' . $this->getDefaultNamespace();
 
-        $namespace .= '\\' . $extra;
+		$namespace .= '\\' . $extra;
 
-        $namespace = str_replace('/', '\\', $namespace);
-        $namespace = str_replace('app\\','', $namespace);
+		$namespace = str_replace('/', '\\', $namespace);
+		$namespace = str_replace('app\\','', $namespace);
 
-        return trim($namespace, '\\');
-    }
+		return trim($namespace, '\\');
+	}
 
-    /**
-     * @return string
-     */
-    protected function getDir(){
-        return $this->laravel['modules']->config('namespace').'/'.str_replace('_', DIRECTORY_SEPARATOR, $this->getModuleName());
-    }
+	/**
+	 * @return string
+	 */
+	protected function getDir(){
+		return str_replace(base_path() . DIRECTORY_SEPARATOR, '', $this->laravel['modules']->config('paths.modules')) . DIRECTORY_SEPARATOR . str_replace('_', DIRECTORY_SEPARATOR, $this->getModuleName());
+	}
 
 }
